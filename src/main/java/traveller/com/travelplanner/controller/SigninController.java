845b9d9bc322a3457fe0f86942abca2a762ab663
luoluojing <83481022+luoluojing@users.noreply.github.com>
2022-01-03 01:50:18 -0500

    Add files via upload

--- src/main/java/traveller/com/travelplanner/controller/SigninController.java
+++ src/main/java/traveller/com/travelplanner/controller/SigninController.java
@@ -1,4 +1,4 @@
-package com.example.travelplandemo1.controller;
+package traveller.com.travelplanner.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
